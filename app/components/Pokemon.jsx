8eb0c8a207006d8063cc6776432996eb84d088e2
jonathandestinec <jonathandestinec@gmail.com>
2024-06-20 00:33:25 +0100

    Done. Next up, add a search feature

--- app/components/Pokemon.jsx
+++ app/components/Pokemon.jsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React, { useEffect, useState } from 'react'
+import { Suspense } from 'react'
 
 function Pokemon() {
 
@@ -27,29 +28,80 @@ function Pokemon() {
         displayPokemon(2)
     }, [])
 
+    const scrollBottomPokeList = () => {
+        const pokeList = document.querySelector('.poke-list')
+        pokeList.scrollTop += 100
+    }
+
+    const scrollTopPokeList = () => {
+        const pokeList = document.querySelector('.poke-list')
+        pokeList.scrollTop = 0
+    }
+
     return (
-        <div className=' flex items-center justify-between w-full'>
+
+        // Style them up
+        <div className=' md:flex block items-center justify-between w-3/5 ml-auto mr-auto poke-container h-4/5 bg-gray-900 bg-opacity-50 absolute transform top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:p-10 p-5 pl-10 pr-10 rounded-lg ring-1 ring-gray-700'>
 
             {/* Display all the pokemons gotten ftom the fetchPokemons */}
 
-            <div className='block w-max'>
+            <div className='block w-max overflow-scroll md:h-full h-2/5 md:m-0 mb-5 relative poke-list scroll-smooth'>
+
+                {/* Mobile scroll notification */}
+                <h4 className=' text-xsm sticky mb-5 text-lime-400 cursor-pointer' onClick={scrollBottomPokeList}>Scroll ▼</h4>
+
                 {pokemons && pokemons.map(pokemon => {
                     const id = pokemon.url.split("/")[6]
                     return (
                         <div key={id}>
-                            <button className=' w-max mb-5' onClick={() => {
+                            <button className=' w-max mb-5 font-mono text-lg underline underline-offset-8' onClick={() => {
                                 displayPokemon(id)
                             }}>{pokemon.name}</button>
                         </div>
                     )
                 })}
+
             </div>
 
             {/* Display Selected pokemon data */}
             {pokemon && (
-                <div>
-                    <h1>{pokemon.species.name}</h1>
-                    <img src={`${pokemon.sprites.front_default}`} alt="" />
+                <div className=' md:flex block items-center justify-between gap-10'>
+
+                    <div className=' md:m-0 mb-5'>
+                        <h3 className=' text-2xl font-mono font-black md:m-0 mb-3'>
+                            <span className=' font-mono text-base font-light mr-5 text-lime-300 italic'>species.name:</span>
+                            <br className=' md:hidden block' />
+                            {pokemon.species.name}
+                        </h3>
+
+                        <h3 className=' text-2xl font-mono font-black md:m-0 mb-3'>
+                            <span className=' font-mono text-base font-light mr-5 text-lime-300 italic'>height:</span>
+                            <br className=' md:hidden block' />
+                            {pokemon.height}
+
+                            {/* Conditional more context about height 🙃 */}
+                            <span className=' font-mono text-sm font-light mr-5 text-amber-300 italic ml-2'>
+                                {pokemon.height > 16 && "(very Tall)"}
+                            </span>
+                        </h3>
+
+                        <h3 className=' text-2xl font-mono font-black md:m-0 mb-3'>
+                            <span className=' font-mono text-base font-light mr-5 text-lime-300 italic'>weight:</span>
+                            <br className=' md:hidden block' />
+                            {pokemon.weight}
+
+                            {/* Conditional more context about weight 🙃 */}
+                            <span className=' font-mono text-sm font-light mr-5 text-amber-300 italic ml-2'>
+                                {pokemon.weight > 900 && "(really heavy)"}
+                            </span>
+                        </h3>
+                    </div>
+
+                    <div>
+                        <img src={`${pokemon.sprites.front_shiny}`} alt="" className=' w-48 -m-4 md:m-0 mr-auto ml-auto' />
+                        <img src={`${pokemon.sprites.back_default}`} alt="" className=' w-48 -m-4 md:block hidden' />
+                    </div>
+
                 </div>
             )}
 
--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,10 @@ import Pokemon from './components/Pokemon'
 
 function page() {
   return (
-    <div>
+    <div className=' w-screen h-screen relative main overflow-hidden'>
+
+        {/* Bg glow */}
+        <div className=' fixed w-60 h-60 bg-blue-900 transform top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-10 rounded-full blur-3xl'></div>
         {/* Pokemon Component */}
         <Pokemon />
         
